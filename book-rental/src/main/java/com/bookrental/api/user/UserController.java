7c9a8f0bc149dbcf0674af0e001e48c4e196b169
Oskar Ożóg <106389146+xDekann@users.noreply.github.com>
2024-11-10 16:18:08 +0100

    nit

--- book-rental/src/main/java/com/bookrental/api/user/UserController.java
+++ book-rental/src/main/java/com/bookrental/api/user/UserController.java
@@ -23,7 +23,6 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Optional;
 
 @Tag(name = "User", description = "User related endpoints")
-
 @RestController
 @RequestMapping("/v1/users")
 public class UserController {
