fe16644e81682f33af2909fcac229e5e93ac853e
Oskar Ożóg <106389146+xDekann@users.noreply.github.com>
2024-11-10 14:50:17 +0100

    sql password adjustment

--- docker/init.sql
+++ docker/init.sql
@@ -4,7 +4,7 @@ CREATE TABLE user (
                       first_name VARCHAR(255) NOT NULL,
                       last_name VARCHAR(255) NOT NULL,
                       email VARCHAR(255) NOT NULL UNIQUE,
-					  password VARCHAR(50) NOT NULL,
+					  password VARCHAR(255) NOT NULL,
                       role VARCHAR(50) DEFAULT 'basic',
                       created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
