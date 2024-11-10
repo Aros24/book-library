ca504603438f16ecba408972a4b77a39230f30a5
Oskar Ożóg <106389146+xDekann@users.noreply.github.com>
2024-11-10 16:36:46 +0100

    Swagger adjustments

--- book-rental/src/main/java/com/bookrental/config/OpenApiConfig.java
+++ book-rental/src/main/java/com/bookrental/config/OpenApiConfig.java
@@ -1,11 +1,36 @@
 package com.bookrental.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfig {
+
+    @Value("${openapi.title}")
+    private String title;
+
+    @Value("${openapi.version}")
+    private String version;
+
+    @Value("${openapi.description}")
+    private String description;
+
+    @Value("${openapi.server.url}")
+    private String serverUrl;
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title(title)
+                        .version(version)
+                        .description(description))
+                .addServersItem(new Server().url(serverUrl));
+    }
 
     @Bean
     public GroupedOpenApi authenticationApi() {
@@ -19,7 +44,7 @@ public class OpenApiConfig {
     public GroupedOpenApi userApi() {
         return GroupedOpenApi.builder()
                 .group("User")
-                .pathsToMatch("/v1/users/**")
+                .pathsToMatch("/v1/users/**", "/v1/users")
                 .build();
     }
 
