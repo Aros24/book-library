4eaf8b8dd1591205301253ed61846e499f0bb4ef
Oskar Ożóg <106389146+xDekann@users.noreply.github.com>
2024-11-10 14:52:04 +0100

    Update UserRepository.java

--- book-rental/src/main/java/com/bookrental/persistence/repositories/UserRepository.java
+++ book-rental/src/main/java/com/bookrental/persistence/repositories/UserRepository.java
@@ -6,10 +6,23 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 
-    @Query(value = "SELECT role FROM user WHERE public_id = :publicId", nativeQuery = true)
-    String getRoleByPublicId(@Param("publicId") String publicId);
+    @Query(value = "SELECT role FROM user WHERE email = :email", nativeQuery = true)
+    String getRoleByEmail(@Param("email") String email);
+
+    boolean existsByEmail(@Param("email") String email);
+
+    @Query(value = "DELETE FROM user WHERE public_id = :publicId", nativeQuery = true)
+    void deleteByPublicId(@Param("publicId") String publicId);
+
+    @Query(value = "SELECT * FROM user WHERE email = :email", nativeQuery = true)
+    Optional<User> findByEmail(@Param("email") String email);
+
+    @Query(value = "SELECT * FROM user WHERE public_id = :publicId", nativeQuery = true)
+    Optional<User> getByPublicId(@Param("publicId") String publicId);
 
 }
