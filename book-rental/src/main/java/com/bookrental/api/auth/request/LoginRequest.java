bd5533f7a262e92c6c04392d09ffeb411b05cd1f
Oskar Ożóg <106389146+xDekann@users.noreply.github.com>
2024-11-23 00:24:07 +0100

    Naming strategy snake case

--- book-rental/src/main/java/com/bookrental/api/auth/request/LoginRequest.java
+++ book-rental/src/main/java/com/bookrental/api/auth/request/LoginRequest.java
@@ -1,6 +1,7 @@
 package com.bookrental.api.auth.request;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
@@ -10,16 +11,15 @@ import lombok.experimental.FieldDefaults;
 
 @Getter
 @Builder
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 @FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
 public class LoginRequest {
 
     @NotNull
-    @JsonProperty("email")
     @Schema(description = "Email address of the user", example = "user@example.com")
     String email;
 
     @NotNull
-    @JsonProperty("password")
     @Schema(description = "Password of the user", example = "password123")
     String password;
 
--- book-rental/src/main/java/com/bookrental/api/user/response/User.java
+++ book-rental/src/main/java/com/bookrental/api/user/response/User.java
@@ -1,5 +1,7 @@
 package com.bookrental.api.user.response;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -8,6 +10,7 @@ import lombok.experimental.FieldDefaults;
 
 @Getter
 @Builder
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 @FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
 public class User {
 
