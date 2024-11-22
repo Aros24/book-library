9bf1c75a14d988c4b875d4ea842f439d371f56ca
Oskar Ożóg <106389146+xDekann@users.noreply.github.com>
2024-11-23 00:22:31 +0100

    Removing public id filter -> adding method and adjusting sec config

--- book-rental/src/main/java/com/bookrental/security/SecurityConfig.java
+++ book-rental/src/main/java/com/bookrental/security/SecurityConfig.java
@@ -1,7 +1,9 @@
 package com.bookrental.security;
 
+import com.bookrental.config.exceptions.ForbiddenException;
+import com.bookrental.config.exceptions.UnauthorizedException;
 import com.bookrental.security.filters.JwtAuthenticationFilter;
-import com.bookrental.security.filters.PublicIdCheckFilter;
+import com.bookrental.security.jwt.JwtAuthenticationToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +11,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.core.GrantedAuthorityDefaults;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -19,12 +23,12 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class SecurityConfig {
 
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
-    private final PublicIdCheckFilter publicIdCheckFilter;
+    private static final String ROLE_ADMIN = "admin";
+    private static final String ROLE_BASIC = "basic";
 
     @Autowired
-    public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter, PublicIdCheckFilter publicIdCheckFilter) {
+    public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter) {
         this.jwtAuthenticationFilter = jwtAuthenticationFilter;
-        this.publicIdCheckFilter = publicIdCheckFilter;
     }
 
     @Bean
@@ -32,10 +36,10 @@ public class SecurityConfig {
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/v1/login", "/auth/v1/register", "/public/**").permitAll()
-                        .requestMatchers("/v1/**").hasAnyRole("basic")
+                        .requestMatchers("/v1/**").hasAnyRole(ROLE_BASIC, ROLE_ADMIN)
+                        .requestMatchers("/v1/users/accounts**").hasAnyRole(ROLE_ADMIN)
                         .anyRequest().authenticated())
-                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
-                .addFilterAfter(publicIdCheckFilter, JwtAuthenticationFilter.class);
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
@@ -48,6 +52,20 @@ public class SecurityConfig {
     @Bean
     public GrantedAuthorityDefaults grantedAuthorityDefaults() {
         return new GrantedAuthorityDefaults("");
+    }
+
+    public static void checkUserAccess(String providedPublicId) throws ForbiddenException {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || !authentication.isAuthenticated()) {
+            throw new UnauthorizedException("User is not authenticated.");
+        }
+
+        JwtAuthenticationToken token = (JwtAuthenticationToken) authentication;
+        String loggedInUserPublicId = token.getPublicId();
+        String loggedInUserRole = token.getRole();
+        if (!loggedInUserPublicId.equals(providedPublicId) && !ROLE_ADMIN.equals(loggedInUserRole)) {
+            throw new ForbiddenException("You do not have permission to access this resource.");
+        }
     }
 
 }
